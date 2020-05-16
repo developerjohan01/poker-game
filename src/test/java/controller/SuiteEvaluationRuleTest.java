01e10a3c55f8b8bc9efd1b2df27993b193543cd6
Johan Nilsson <developerjohan01@gmail.com>
2020-05-16 22:31:02 +0200

    WIP - Setting up anoter test which will have to be fixed

---
+++ src/test/java/controller/SuiteEvaluationRuleTest.java
@@ -0,0 +1,37 @@
+package controller;
+
+import entity.CardHand;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SuiteEvaluationRuleTest {
+
+    EvaluationRule rankRule;
+    Game game; // the context in which the hand is used
+    CardHand hand;
+    String[] mixedSuiteHand = {"AS", "10C", "10H", "3D", "3S"};
+    String[] sameSuiteHand = {"AS", "10S", "9S", "3S", "2S"};
+
+    @BeforeEach
+    void setUp() {
+        game = Game.newInstance();
+        rankRule = new SuiteEvaluationRule();
+    }
+
+    @Test
+    void mixedSuiteEvaluate() {
+        hand = game.getHandOfCardsFromInput(mixedSuiteHand);
+        String evaluation = rankRule.evaluate(hand);
+        assertEquals("", evaluation);
+    }
+
+    @Test
+    void sameSuiteEvaluate() {
+        hand = game.getHandOfCardsFromInput(sameSuiteHand);
+        String evaluation = rankRule.evaluate(hand);
+        assertEquals("Flush of Spades", evaluation);
+    }
+
+}
