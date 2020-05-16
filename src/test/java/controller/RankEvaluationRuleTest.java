0a3a1319380d40508ad2153bc3e9ec8ab2c974c8
Johan Nilsson <developerjohan01@gmail.com>
2020-05-16 22:20:47 +0200

    WIP - Setting up the test which will have to be fixed

--- src/test/java/controller/RankEvaluationRuleTest.java
+++ src/test/java/controller/RankEvaluationRuleTest.java
@@ -1,5 +1,6 @@
 package controller;
 
+import entity.CardHand;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,12 +8,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RankEvaluationRuleTest {
 
+    EvaluationRule rankRule;
+    Game game; // the context in which the hand is used
+    CardHand hand;
+    String[] testHand = {"AS", "10C", "10H", "3D", "3S"};
+
     @BeforeEach
     void setUp() {
+        game = Game.newInstance();
+        hand = game.getHandOfCardsFromInput(testHand);
+        rankRule = new RankEvaluationRule();
     }
 
     @Test
     void evaluate() {
-        fail("todo");
+        String evaluation = rankRule.evaluate(hand);
+        assertEquals("Two pairs", evaluation);
     }
 }
